ebcbcc56577485299d127f6f45eda054a76cf46b
ShivshankarKushwaha <shivshankarkushwaha0000@gmail.com>
2023-10-15 12:05:38 +0530

    offline working

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,28 +1,58 @@
-const CACHE_NAME ='version-1';
-const urltoCache =['index.html','offline.html'];
-this.addEventListener('install',(e)=>
+const CACHE_NAME = "version-1";
+const urltoCache = ["index.html", "offline.html", './src/App.js', './src/App.jsx', './src/App.ts', './src/App.tsx'];
+this.addEventListener("install", (e) =>
 {
     e.waitUntil(
-        caches.open(CACHE_NAME).then(cache=>
-            {
-                console.log('caches opened');
-                return cache.addAll(urltoCache);
-            })
-    )
-})
-this.addEventListener('fetch',(e)=>
+        caches.open(CACHE_NAME).then((cache) =>
+        {
+            console.log("caches opened");
+            return cache.addAll(urltoCache);
+        }),
+    );
+});
+this.addEventListener("fetch", (e) =>
 {
     e.respondWith(
-        caches.match(e.request).then(res=>
-            {
-                return fetch(e.request).catch(()=>{caches.match('offline.html')})
-            })
-    )
-})
+        caches.match(e.request).then((res) =>
+        {
+            if (res) {
+                return res; // Return the cached resource if it exists
+            }
+            return fetch(e.request)
+                .then((response) =>
+                {
+                    if (!response || response.status !== 200 || response.type !== "basic") {
+                        return response;
+                    }
 
-this.addEventListener('activate',(e)=>
+                    const responseToCache = response.clone();
+
+                    caches.open(CACHE_NAME).then((cache) =>
+                    {
+                        cache.put(e.request, responseToCache); // Cache the fetched response
+                    });
+
+                    return response;
+                })
+                .catch(() => caches.match("offline.html")); // Return the offline page if the network request fails
+        }),
+    );
+});
+
+this.addEventListener("activate", (e) =>
 {
-    const cacheWhiteList=[];
+    const cacheWhiteList = [];
     cacheWhiteList.push(CACHE_NAME);
-    e.waitUntil(caches.keys().then(cachenames=>Promise.all(cachenames.map(cachename=>{if(!cacheWhiteList.includes(cachename)){return caches.delete(cachename)}}))))
-})
+    e.waitUntil(
+        caches.keys().then((cachenames) =>
+            Promise.all(
+                cachenames.map((cachename) =>
+                {
+                    if (!cacheWhiteList.includes(cachename)) {
+                        return caches.delete(cachename);
+                    }
+                }),
+            ),
+        ),
+    );
+});
